246f5031ce14a5440215e701b157605382f4c8de
walid-chihab <walid-chihab@users.noreply.github.com>
2025-05-04 21:58:30 +0100

    debuging ooooof

--- Data_Structures/Linked_List/ex0_singly_patient.c
+++ Data_Structures/Linked_List/ex0_singly_patient.c
@@ -7,9 +7,69 @@
 /*	3. Ajouter une fonction qui permet de chercher le patient le plus âgé.				*/
 /*	4. Ecrire le programme appelant.							        */
 /********************************************************************************************************/
-
+#include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
 #include <string.h>
 
+typedef struct patient {
+	char nom [50];
+	char prenom [70];
+	int age ;
+	struct patient * suivant;
+}patient;
 
+patient *cree_patient(char _nom[],char _prenom[],int _age){
+	patient *nv_patient = (patient*) malloc(sizeof(patient));
+	if (nv_patient == NULL){
+		printf ("erreure d'allocation");
+		exit(1);
+	}
+	strcpy(nv_patient->nom, _nom);
+	strcpy(nv_patient->prenom, _prenom);
+	nv_patient->age = _age;
+	nv_patient->suivant = NULL;
+	return nv_patient;
+}
+
+void	affich_patient(patient *tete){
+	patient *courant = tete;
+	int i = 0;
+	while (courant != NULL ){
+		printf ("patient(%d)  M:%s - %s d'age %d \n",i+1,courant->nom,courant->prenom,courant->age);
+		i++;
+		courant=courant->suivant;
+	}
+}
+
+void  age_patient(patient *tete){
+	patient *courant = tete;
+	int max_age = -1;
+	while (courant != NULL){
+		if (courant->age > max_age){
+			max_age = courant->age;
+		}
+		courant=courant->suivant;
+	}
+	printf("le plus grand patient est %s -%s de %d ans",courant->nom,courant->prenom,courant->age);
+}
+
+int main (){
+	
+	patient *tete = NULL;
+	patient *premier;
+	patient *deuxieme;
+      
+	premier= cree_patient("walid","chihab",20 );
+	deuxieme= cree_patient("anas","glioula",21);
+
+	tete = premier;
+
+	premier->suivant=deuxieme;
+	deuxieme->suivant=NULL;
+
+	affich_patient(tete);
+	printf("---------------------");
+	age_patient(tete);
+	return 0;
+}
