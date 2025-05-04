cf3b0e8d6dbb86311eadf85987e6cb3c59d87270
walid-chihab <walid-chihab@users.noreply.github.com>
2025-05-04 18:57:02 +0100

    verifiant git

---
+++ Data_Structures/Linked_List/ex0_singly_patient.c
@@ -0,0 +1,15 @@
+/********************************************************************************************************/
+/*	On considère l’ensemble des patients qui arrivent chez un médecin, à priori on ne		*/
+/*	connaît pas le nombre de personnes qui peuvent joindre le cabinet. C’est pour cela on		*/
+/*	préfère utiliser les listes chainées. A l’entrée on note le nom, le prénom et l’âge du patient.	*/
+/*	1. Ecrire la fonction qui permet d’enregistrer les informations des patients.			*/
+/*	2. Ajouter une fonction qui permet de lister l’ensemble des patients.				*/
+/*	3. Ajouter une fonction qui permet de chercher le patient le plus âgé.				*/
+/*	4. Ecrire le programme appelant.							        */
+/********************************************************************************************************/
+
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+
+
