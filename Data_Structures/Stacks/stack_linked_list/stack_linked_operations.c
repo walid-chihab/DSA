1dbf430d4856e926e89ef4941323534f1ddbd9b9
walid-chihab <walid-chihab@users.noreply.github.com>
2025-05-27 02:04:31 +0100

    nice

---
+++ Data_Structures/Stacks/stack_linked_list/stack_linked_operations.c
@@ -0,0 +1,48 @@
+/*      
+*	    _____        _____________
+*	   /     |      /             | 
+*	  /      ▼     /              ▼
+*	 E     │  'E'   │ ← top       E
+*	       ├────────┤ 
+*	       │  'D'   │
+*	       ├────────┤
+* 	       │  'C'   │
+*              ├────────┤
+* 	       │  'B'   │
+*    	       ├────────┤
+*	       │  'A'   │ ← Base de la pile
+*	       └────────┘
+*
+*           First In, Last Out
+*         
+*                 filo
+*/	
+
+
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+// 1
+typedef struct Node {
+    char data;
+    struct Node *next;
+} Node;
+
+// 2.initialisation du pile par null
+Node* init_stack() {
+    return NULL;
+}
+
+//3.Empiler
+Node* push(Node* top, char value) {
+    Node* newNode = (Node*) malloc(sizeof(Node));
+    if (!newNode) {
+        printf("Erreur d'allocation mémoire\n");
+        exit(1);
+    }
+    newNode->data = value;
+    newNode->next = top;
+    return newNode;
+}
